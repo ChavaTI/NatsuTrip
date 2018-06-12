a2bca29f9b2210b96451869ced7fc856f40b9e51
Salvador <chavatloz@gmail.com>
2018-06-11 23:46:04 -0500

    implementacion de express-session

--- src/config/server.js
+++ src/config/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
+const session = require('express-session');
 
 
 const app = express();
@@ -16,6 +17,11 @@ app.set('views', path.join(__dirname, '../app/views'));
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
 app.use('/public',express.static(path.join(__dirname, '../app/public')));
+app.use(session({
+    secret: 'qwerty12345',
+    resave: false,
+    saveUninitialized: false
+}));
 
 
 module.exports = app;
